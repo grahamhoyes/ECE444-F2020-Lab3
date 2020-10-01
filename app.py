b23150e6ddb3467451ad8e102d00e22388ba6701
Graham Hoyes <accounts@grahamhoyes.com>
2020-09-30 23:03:33 -0400

    Add email to name form

--- app.py
+++ app.py
@@ -1,18 +1,44 @@
 from datetime import datetime
+import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 
+from forms import NameForm
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "123456")
+
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html", current_time=datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get("name")
+        if old_name is not None and old_name != form.name.data:
+            flash("Looks like you have changed your name!")
+
+        session["name"] = form.name.data
+
+        old_email = session.get("email")
+        if old_email is not None and old_email != form.email.data:
+            flash("Looks like you have changed your email!")
+
+        session["email"] = form.email.data
+
+        return redirect(url_for("index"))
+
+    return render_template(
+        "index.html",
+        form=form,
+        name=session.get("name"),
+        email=session.get("email"),
+        current_time=datetime.utcnow(),
+    )
 
 
 @app.route("/user/<name>/")
---
+++ forms.py
@@ -0,0 +1,17 @@
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required, Email, ValidationError
+
+
+class NameForm(Form):
+    name = StringField("What is your name?", validators=[Required()])
+    email = StringField(
+        "What is your UofT email address?",
+        render_kw={"type": "email"},
+        validators=[Required(), Email()],
+    )
+    submit = SubmitField("Submit")
+
+    def validate_email(self, field):
+        if not field.data.endswith("utoronto.ca"):
+            raise ValidationError("Please use your UofT email.")
